69fafd81fb5ce08589a24193fbe29a4fa884b12b
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2020-06-02 16:31:34 +0200

    Implemented cron group, reindex command, indexers killing

---
+++ Command/Magento/Reindex.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace MageSuite\Importer\Command\Magento;
+
+class Reindex implements \MageSuite\Importer\Command\Command
+{
+    /**
+     * @var \Magento\Indexer\Model\IndexerFactory
+     */
+    protected $indexerFactory;
+
+    /**
+     * @var \Magento\Indexer\Model\Indexer\CollectionFactory
+     */
+    protected $indexerCollectionFactory;
+
+    public function __construct(
+        \Magento\Indexer\Model\IndexerFactory $indexerFactory,
+        \Magento\Indexer\Model\Indexer\CollectionFactory $indexerCollectionFactory
+    )
+    {
+        $this->indexerFactory = $indexerFactory;
+        $this->indexerCollectionFactory = $indexerCollectionFactory;
+    }
+
+    public function execute($configuration)
+    {
+        $indexes = isset($configuration['indexes']) ? $configuration['indexes'] : $this->getAllAvailableIndexes();
+
+        $output = '';
+
+        foreach($indexes as $indexId) {
+            $indexer = $this->indexerFactory->create();
+
+            $indexer->load($indexId);
+
+            $startTime = microtime(true);
+
+            $indexer->getState()->setStatus(\Magento\Framework\Indexer\StateInterface::STATUS_VALID);
+            $indexer->getState()->save();
+
+            $indexer->reindexAll();
+
+            $elapsed = microtime(true) - $startTime;
+
+            $output .= $indexer->getTitle() . ' index has been rebuilt successfully in ' . gmdate('H:i:s', $elapsed).PHP_EOL;
+        }
+
+        return $output;
+    }
+
+    protected function getAllAvailableIndexes()
+    {
+        $collection = $this->indexerCollectionFactory->create();
+
+        return $collection->getAllIds();
+    }
+}
---
+++ Model/KillIndexers.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace MageSuite\Importer\Model;
+
+class KillIndexers
+{
+    /**
+     * @var \Magento\Framework\DB\Adapter\AdapterInterface
+     */
+    protected $connection;
+
+    public function __construct(\Magento\Framework\App\ResourceConnection $resourceConnection)
+    {
+        $this->connection = $resourceConnection->getConnection();
+    }
+
+    public function execute() {
+        $this->killAll('group=index');
+
+        $this->connection->update(
+            $this->connection->getTableName('mview_state'),
+            ['status' => 'idle']
+        );
+
+        $this->connection->update(
+            $this->connection->getTableName('cron_schedule'),
+            [
+                'status' => 'error',
+                'messages' => 'Killed by importer'
+            ],
+            [
+                'job_code IN (?)' => ['indexer_update_all_views','indexer_reindex_all_invalid'],
+                'status = ?' => 'running'
+            ]
+        );
+    }
+
+    protected function killAll($match) {
+        $match = escapeshellarg($match);
+
+        exec("ps x|grep $match|grep -v grep|awk '{print $1}'", $pids, $ret);
+
+        if($ret) {
+            return '';
+        }
+
+        if(empty($pids)) {
+            return '';
+        }
+
+        foreach($pids as $pid) {
+            if(preg_match('/^([0-9]+)/', $pid, $r)) {
+                system('kill -9 '. $r[1], $k);
+            }
+        }
+    }
+
+}
