f2e68362dece024ab2e19535bad692f30d0c68bf
Iwona Jóźwiak <iwona.jozwiak@creativestyle.pl>
2023-04-18 10:58:52 +0200

    feat: [ROT-1270] Move checki isIndexerEnabled method to configuration helper

--- Plugin/DisableIndexer.php
+++ Plugin/DisableIndexer.php
@@ -4,29 +4,17 @@ namespace MageSuite\Importer\Plugin;
 
 class DisableIndexer
 {
-    const INDEXER_ENABLED_XML_SECTION = 'indexer/indexing';
+    protected \MageSuite\Importer\Helper\Config $configuration;
 
-    const INDEXER_ENABLED_XML_PATH = 'indexer/indexing/enabled';
-
-    /**
-     * @var \Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory
-     */
-    protected $configCollectionFactory;
-
-    public function __construct(\Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory $configCollectionFactory)
-    {
-        $this->configCollectionFactory = $configCollectionFactory;
-    }
-
-    public function isIndexerEnabled()
-    {
-        $indexerConfig = $this->getIndexerConfigFromDatabase();
-        return empty($indexerConfig) ? false : $indexerConfig->getValue() === '1';
+    public function __construct(
+        \MageSuite\Importer\Helper\Config $configuration
+    ) {
+        $this->configuration = $configuration;
     }
 
     public function aroundUpdateMview(\Magento\Indexer\Model\Processor $subject, callable $proceed)
     {
-        if(!$this->isIndexerEnabled()) {
+        if(!$this->configuration->isIndexerEnabled()) {
             throw new \Exception("Indexers are disabled");
         }
 
@@ -35,22 +23,10 @@ class DisableIndexer
 
     public function aroundReindexAllInvalid(\Magento\Indexer\Model\Processor $subject, callable $proceed)
     {
-        if(!$this->isIndexerEnabled()) {
+        if(!$this->configuration->isIndexerEnabled()) {
             throw new \Exception("Indexers are disabled");
         }
 
         return $proceed();
-    }
-
-    protected function getIndexerConfigFromDatabase()
-    {
-        $configCollection = $this->configCollectionFactory->create();
-        $configCollection->addScopeFilter(
-            \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
-            0,
-            self::INDEXER_ENABLED_XML_SECTION
-        )->addFieldToFilter('path', ['eq' => self::INDEXER_ENABLED_XML_PATH]);
-
-        return current($configCollection->getItems());
     }
 }
